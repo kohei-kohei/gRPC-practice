441cf4e5666717553ceb4c52101679f64aacca2b
kohei-kohei <sky6sk212@gmail.com>
2021-03-28 19:49:16 +0900

    feat: client web server

--- client/client.py
+++ client/client.py
@@ -5,16 +5,25 @@ sys.path.append('../gacha')
 import grpc
 import gacha_pb2
 import gacha_pb2_grpc
+from flask import Flask, render_template, request
 
-def gacha_server(stub, count):
-    response = stub.GachaResult(gacha_pb2.Request(count=count))
-    print('Response from server: {}'.format(response.result))
-
-def run():
+def gacha_server(count):
     with grpc.insecure_channel('localhost:9000') as channel:
         stub = gacha_pb2_grpc.GachaStub(channel)
-        count = input("ガチャを回す回数は? > ")
-        gacha_server(stub, count)
+        response = stub.GachaResult(gacha_pb2.Request(count=count))
+    
+    return response.result 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET"])
+def login_manager():
+    results = gacha_server(request.args.get("count"))
+    return render_template("result.html", results=results)
 
 if __name__ == '__main__':
-    run()
+    app.run(port=8000)
